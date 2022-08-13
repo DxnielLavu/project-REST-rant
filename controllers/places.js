196ce92619db78f558807cf9466b82848b95464a
Daniel <dbaray2118@gmail.com>
2022-08-13 13:03:33 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,43 +1,55 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
+const router = require("express").Router();
+const places = require("../models/places.js");
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = "Anytown";
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = "USA";
   }
-  places.push(req.body)
-  res.redirect('/places')
-})
-
+  places.push(req.body);
+  res.redirect("/places");
+});
 
 // GET /places
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]      
-      res.render('places/index', { places })
-  })
-  
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "http://placekitten.com/250/250",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "http://placekitten.com/250/250",
+    },
+  ];
+  res.render("places/index", { places });
+});
 
-module.exports = router
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id] });
+  }
+});
+
+module.exports = router;
