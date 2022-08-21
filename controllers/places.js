72e1d1f1abb06120f125e3f2d613372c785044c7
Daniel <dbaray2118@gmail.com>
2022-08-20 19:28:04 -0700

    rest-rant part 10

--- controllers/places.js
+++ controllers/places.js
@@ -39,14 +39,16 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    db.Place.findOne({ _id: req.params.id })
-        .then(place => {
-            res.render('places/show', { place })
-        })
-        .catch(err => {
-            console.log('err', err)
-            res.render('error404')
-        })
+    db.Place.findById(req.params.id)
+    .populate('comments')
+    .then(place => {
+        console.log(place.comments)
+        res.render('places/show', { place })
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
 })
 
 router.put('/:id', (req, res) => {
@@ -81,12 +83,37 @@ router.get('/:id/edit', (req, res) => {
         })
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+router.delete('/:id/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(() => {
+            console.log('Success')
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 module.exports = router
